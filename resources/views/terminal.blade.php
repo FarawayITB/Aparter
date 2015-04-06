7293fa5e4d59c7ff93cd9fdfb78c93f75cbc95e3
Muhammad Husain Jakfari <emha.husain104@gmail.com>
2015-04-06 21:38:46 +0700

    hmm3

--- resources/views/terminal.blade.php
+++ resources/views/terminal.blade.php
@@ -1,12 +1,24 @@
-@extends('header_footer')
+<!DOCTYPE html>
+@extends('header_footer2')
 
-@section('content')
-<html>
-	<head>
-
-	</head>
-<body>
-	hohohoho
-</body>
-</html>
+@section('content2')
+<div class="main"><!-- start main -->
+<div class="container">
+	<div class="features"><!-- start feature -->
+		<h1>Terminal</h1>
+		<div class="row features_list1"><!-- start feature -->
+			
+			@foreach ($allTerminal as $terminal)
+			<div class="col-md-3 feature">
+				<div class="fancyDemo">
+					<a rel="group" title="" href="{{ asset('images/ser_pic5.jpg')}}"><img src="{{ asset('images/ser_pic5.jpg')}}" alt=""class="img-responsive"/></a>
+				</div>
+				<a href="single-page.html">{{ $terminal->nama }}</a>
+				<p class="para">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+			</div>
+			@endforeach
+		</div>
+	</div><!-- end feature -->
+</div>
+</div>
 @endsection
