acd318eff92ff5ae3b2c8a45873aa4dbfe425a9d
Rikysamuel <rikz.samuel@gmail.com>
2015-04-23 19:20:48 +0700

    migration

--- database/migrations/2015_04_22_151203_reset_all_db.php
+++ database/migrations/2015_04_22_151203_reset_all_db.php
@@ -15,23 +15,19 @@ class ResetAllDb extends Migration {
 
 		Schema::create('kecamatan', function($table)
 		{
-			$table->integer('id_kecamatan')->unsigned();
+			$table->increments('id_kecamatan');
 			$table->string('nama_kecamatan');
-			
-			$table->primary('id_kecamatan');
 		});
 
 		Schema::create('jenis_kendaraan', function($table)
 		{
-			$table->integer('id_jenis_kendaraan')->unsigned();
+			$table->increments('id_jenis_kendaraan');
 			$table->string('jenis_kendaraan_parkir');
-
-			$table->primary('id_jenis_kendaraan');
 		});
 
 		Schema::create('parkir', function($table)
 		{
-			$table->string('id_parkir');
+			$table->increments('id_parkir');
 			$table->string('id_pemilik');
 			$table->integer('id_kecamatan')->unsigned();
 			$table->integer('id_jenis_kendaraan')->unsigned();
@@ -41,45 +37,40 @@ class ResetAllDb extends Migration {
 			$table->float('luas');
 			$table->integer('tarif')->unsigned();
 
-			$table->primary('id_parkir');
 			$table->foreign('id_kecamatan')->references('id_kecamatan')->on('kecamatan')->onDelete('cascade');
 			$table->foreign('id_jenis_kendaraan')->references('id_jenis_kendaraan')->on('jenis_kendaraan')->onDelete('cascade');
 		});
 
 		Schema::create('terminal', function($table)
 		{
-			$table->string('id_terminal');
+			$table->increments('id_terminal');
 			$table->string('nama');
 			$table->string('alamat');
 			$table->string('lokasi');
 			$table->integer('jumlah_lahan')->unsigned();
-
-			$table->primary('id_terminal');
 		});
 
 		Schema::create('lahan', function($table)
 		{
-			$table->string('id_lahan');
+			$table->increments('id_lahan');
 			$table->double('luas');
-			$table->string('id_terminal');
+			$table->integer('id_terminal')->unsigned();
 			$table->string('id_pemilik');
 			$table->string('status');
 			$table->integer('harga')->unsigned();
 
-			$table->primary('id_lahan');
 			$table->foreign('id_terminal')->references('id_terminal')->on('terminal')->onDelete('cascade');
 
 		});
 
 		Schema::create('pembayaran', function($table)
 		{
-			$table->integer('id_pembayaran')->unsigned();
-			$table->string('id_tempat_parkir')->nullable();
-			$table->string('id_tempat_lahan')->nullable();
+			$table->increments('id_pembayaran');
+			$table->integer('id_tempat_parkir')->unsigned()->nullable();
+			$table->integer('id_tempat_lahan')->unsigned()->nullable();
 			$table->string('periode');
 			$table->string('gambar');
 
-			$table->primary('id_pembayaran');
 			$table->foreign('id_tempat_parkir')->references('id_parkir')->on('parkir')->onDelete('cascade');
 			$table->foreign('id_tempat_lahan')->references('id_lahan')->on('lahan')->onDelete('cascade');
 		});
