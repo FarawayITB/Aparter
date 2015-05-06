700adcaf5cc6936a6659f8418a6a587735dc7091
Rikysamuel <rikz.samuel@gmail.com>
2015-05-06 23:17:16 +0700

    login

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
 	$('#logoutLink').click(function(e) {
+		alert("test");
 		$.ajax({
 			type: 'get',
 			url: 'http://e-gov-bandung.tk/dukcapil/api/public/auth/logout',
 			success: function(data) {
+				console.log("success");
 			},
 			error: function(data) {
 				// alert(data);
