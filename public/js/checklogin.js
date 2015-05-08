7984fcc6ec2e346214d9c89762a6b14c96dd7c24
Rikysamuel <rikz.samuel@gmail.com>
2015-05-08 17:52:11 +0700

    check login

--- public/js/checklogin.js
+++ public/js/checklogin.js
@@ -1,21 +1,2 @@
 $(document).ready(function () {
-	function check(){
-		$.ajax({
-		    type: 'get',
-		    url: 'http://e-gov-bandung.tk/dukcapil/api/public/check/authenticated',
-		    success: function(data) {
-		    	console.log(data)
-		    	if (data != 'false') { //redirect ke home page kalian, tp kalian juga harus login sendiri juga
-		    		var url = "http://e-gov-bandung.tk/aparter/public/home?id=" + data;
-		    		window.location.href = url;
-		    	} else { //redirect ke alamat login kalian
-		    		var url = "http://e-gov-bandung.tk/aparter/public/" 
-		    		window.location.href = url
-		    	}
-		    },
-		    error: function(data) {
-		    	alert(data);
-		    }
-		});
-	}
 });
