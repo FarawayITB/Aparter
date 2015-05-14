9add486eb19016b4e7b29174c46624240b136f76
Aurelia <aurellerua@ymail.com>
2015-05-14 17:18:34 +0700

    about

---
+++ app/Http/Controllers/NotifikasiviewController.php
@@ -0,0 +1,22 @@
+<?php namespace App\Http\Controllers;
+
+use View;
+use \App\Notification;
+
+use Carbon\Carbon;
+use Input;
+use Redirect;
+
+class NotifikasiviewController extends Controller {
+    public function home()
+    {
+        return View::make('notifikasi');
+	}
+
+	public function test(){
+		Notification::cekTenggat();
+		$allNotif = Notification::get()->where('id_ktp', '=', 'budi');
+		
+		return view('notifikasi',  ["allNotif" => $allNotif]);
+	}
+}
