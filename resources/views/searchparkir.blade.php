6c6f001c1d629aa4615394df26822540e0d3540c
Rakhmatullah Yoga Sutrisna <13512053@std.stei.itb.ac.id>
2015-05-16 16:14:12 +0700

    penanganan isEmpty parkir

--- resources/views/searchparkir.blade.php
+++ resources/views/searchparkir.blade.php
@@ -24,12 +24,14 @@
 			<?php }
 			foreach ($parkir as $par): ?>
 				<?php $location = explode(",", $par->lokasi)?>
-				<div class="col-md-6" id="parkir_item" onclick="setMarker(<?php echo $location[0]?>,<?php echo $location[1]?>)">
-					<h3><a href="javascript:;">Parkir <?php echo $par->alamat ?></a></h3>
-					<dl>
-						<p>Rp <?php echo $par->tarif ?> per hari | <?php echo $par->status ?></p>
-					</dl>
-				</div>
+				<?php if ($par->status==4) {?>
+					<div class="col-md-6" id="parkir_item" onclick="setMarker(<?php echo $location[0]?>,<?php echo $location[1]?>)">
+						<h3><a href="javascript:;">Parkir <?php echo $par->alamat ?></a></h3>
+						<dl>
+							<p>Rp <?php echo $par->tarif ?> per hari | <?php echo $par->status ?></p>
+						</dl>
+					</div>
+				<?php } ?>
 			<?php endforeach ?>
 		</div>
 	</div>
