e5f8ec1db4ffc882e3e20931372e5fa0038802ee
Rakhmatullah Yoga Sutrisna <13512053@std.stei.itb.ac.id>
2015-05-16 15:00:27 +0700

    penanganan isEmpty parkir

--- resources/views/parkir.blade.php
+++ resources/views/parkir.blade.php
@@ -22,17 +22,15 @@
 			<?php if ($parkir->count()==0) { ?>
 				<h3>Tidak tersedia parkir pada kecamatan yang Anda masukkan</h3>
 			<?php }
-			else {
-				foreach ($parkir as $par):
-					$location = explode(",", $par->lokasi);?>
-					<div class="col-md-6" id="parkir_item" onclick="setMarker(<?php echo $location[0]?>,<?php echo $location[1]?>)">
-						<h3><a href="javascript:;">Parkir <?php echo $par->alamat ?></a></h3>
-						<dl>
-							<p>Rp <?php echo $par->tarif ?> per hari | <?php echo $par->status ?></p>
-						</dl>
-					</div>
-				<?php endforeach
-			} ?>
+			foreach ($parkir as $par): ?>
+				<?php $location = explode(",", $par->lokasi)?>
+				<div class="col-md-6" id="parkir_item" onclick="setMarker(<?php echo $location[0]?>,<?php echo $location[1]?>)">
+					<h3><a href="javascript:;">Parkir <?php echo $par->alamat ?></a></h3>
+					<dl>
+						<p>Rp <?php echo $par->tarif ?> per hari | <?php echo $par->status ?></p>
+					</dl>
+				</div>
+			<?php endforeach ?>
 		</div>
 	</div>
 @endsection
