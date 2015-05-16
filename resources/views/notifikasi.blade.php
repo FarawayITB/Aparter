22c6ebed32490290812eb2025e93c71f5f4d2d72
Rikysamuel <rikz.samuel@gmail.com>
2015-05-16 17:06:25 +0700

    bug fix

--- resources/views/notifikasi.blade.php
+++ resources/views/notifikasi.blade.php
@@ -15,7 +15,7 @@
 	</div>
 
 	@foreach ($allReminder as $reminder)
-	 <div class="alert alert-success alert-dismissable"> 
+	 <div class="alert alert-danger alert-dismissable"> 
 			<button type="button" class="close" data-dismiss="alert" aria-hidden="true"> &times; 
 			</button> {{$reminder->body}}
 	</div>
