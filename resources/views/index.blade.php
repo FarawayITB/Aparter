942427b0f7fe7e1711be1cf08c3fb51374a09213
Rikysamuel <rikz.samuel@gmail.com>
2015-05-18 12:33:59 +0700

    view:

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,26 +7,17 @@
 						<div class="col-md-4 grid1_of_3">
 							  <h2>Parkir</h2>
 							  <img src="images/icon1.png" class="img-responsive"/>
-							  <p>Pada menu ini, anda dapat melakukan pencarian terhadap tempat parkir di area tertentu. Hanya tinggal memasukan nama daerah pada box "Parkiran di Kecamatan" lalu klik tombol "Search"!</p>
-						     <div class="rd_more1">
-								<a href="single-page.html"><button class="btn_style">view more</button></a>
-							</div>					
+							  <p>Pada menu ini, anda dapat melakukan pencarian terhadap tempat parkir di area tertentu. Hanya tinggal memasukan nama daerah pada box "Parkiran di Kecamatan" lalu klik tombol "Search"!</p>					
 						</div>
 						<div class="col-md-4 grid1_of_3">
 							<h2>Terminal</h2>
 							  <img src="images/icon2.png" class="img-responsive"/>
-							  <p>Pada menu ini, anda dapat melakukan pencarian lahan dan melakukan sewa terhadap lahan yang anda sukai. Hanya tinggal masuk kedalam menu Terminal dan pilih Terminal tertentu dan mulai sewa lahan!</p>
-						      <div class="rd_more1">
-								<a href="single-page.html"><button class="btn_style">view more</button></a>
-							</div>					
+							  <p>Pada menu ini, anda dapat melakukan pencarian lahan dan melakukan sewa terhadap lahan yang anda sukai. Hanya tinggal masuk kedalam menu Terminal dan pilih Terminal tertentu dan mulai sewa lahan!</p>			
 						</div>
 						<div class="col-md-4 grid1_of_3">
 							<h2>Aksi</h2>
 							  <img src="images/icon3.png" class="img-responsive"/>
 							  <p>Pada menu ini, tersedia berbagai macam aksi yang dapat anda lakukan. Mulai dari mengunggah bukti pembayaran, notifikasi, mendaftarkan lahan parkir, dll.</p>
-						     <div class="rd_more1">
-								<a href="single-page.html"><button class="btn_style">view more</button></a>
-							</div>	
 						</div>
 					    <div class="clearfix"></div>
 			</div>
